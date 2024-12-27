1ee80e2352a2c6df0f79add42dc6ec969409e729
R Kataria <151474794+r-kataria@users.noreply.github.com>
2024-12-27 17:17:57 +0530

    refactor: use learnopengl as base; add: hdr and antialiasing

--- todhchai/helpers/texture.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <glad/gl.h>
-
-// Load a 2D texture from file, optionally repeating or clamping
-GLuint LoadTextureTileBox(const char *texture_file_path, bool doRepeat, bool flip);
